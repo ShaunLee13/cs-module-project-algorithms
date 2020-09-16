51334078a2dc4ece44d773411479706179809d59
Shaun Lee <shaundlee.0103@gmail.com>
2020-09-16 15:38:21 -0400

    mod single

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,10 +3,14 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
-
-    pass
-
+    # loop through every item in our list
+    for i in range(len(arr)):
+        # use python's count method to check whether an item
+        # has only one occurence
+        # if the check has a value of one,
+        # we can return the value at this index
+        if arr.count(arr[i]) == 1:
+            return arr[i]
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
